968e616f1a35f590277afd1bc70376192f541aea
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-09-29 21:56:39 +0400

    styling events

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -11,21 +11,29 @@ const EventItem = (props: OneEventProps) => {
     year: "numeric",
   });
 
+  const formattedAdress = location.replace(", ", "\n");
+
+  const exploreLink = `/events/${id}`;
+
   return (
-    <li>
-      <img src="" alt="   " />
-      <div>
-        <div>
-          <h2>TITLE</h2>
-          <div>
-            <time></time>
+    <li className="sm:flex-row shadow-lg rounded-[8px] overflow-hidden bg-white m-[1rem] flex flex-col gap-[1rem] sm:pb-0 pb-3">
+      <img
+        src={`/${image}`}
+        alt={title}
+        className="object-cover overflow-hidden w-full h-[10rem] rounded-[8px] sm:w-[40%] sm:h-[14rem]"
+      />
+      <div className="w-full py-o px-[1rem] sm:w-[60%] p-0 text-left">
+        <div className="">
+          <h2 className="my-[0.5rem] sm:my-[1rem]">{title}</h2>
+          <div className="flex gap-[0.5rem] items-center">
+            <time className="text-gray-600 font-bold">{humanReadableDate}</time>
           </div>
-          <div>
-            <h1></h1>
+          <div className="flex gap-[0.5rem] items-center">
+            <h1 className="my-[0.5rem] text-gray-600">{formattedAdress}</h1>
           </div>
         </div>
-        <div>
-          <Link href="/">Explore Event</Link>
+        <div className="underline text-right text-lg">
+          <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
     </li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import { getFeaturedEvents } from "../utils/dummyData";
+import EventList from "../components/events/EventList";
 
 const Home: NextPage = () => {
   const featuredEvents = getFeaturedEvents();
 
-  return <div>sad</div>;
+  return (
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
+  );
 };
 
 export default Home;
